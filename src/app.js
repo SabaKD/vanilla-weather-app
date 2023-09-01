95d020ea56b878026017bad654a5e1540b7e7be4
SabaKD <saba.zadeh96@gmail.com>
2023-09-01 18:53:04 +0200

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp){
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours< 10){
-        hours = `0 ${hours}`;
+        hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
     if (minutes< 10){
-        minutes = `0 ${minutes}`;
+        minutes = `0${minutes}`;
     }
     
     let day = date.getDay();
@@ -16,15 +16,16 @@ function formatDate(timestamp){
 }
 
 
-function displayTemprature(response){
-    let tempratureElement = document.querySelector("#temprature");
+function displayTemperature(response){
+    let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
-    tempratureElement.innerHTML = Math.round(response.data.temperature.current);
+    celciusTemperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `Humidity : ${response.data.temperature.humidity}%`;
@@ -32,12 +33,13 @@ function displayTemprature(response){
     dateElement.innerHTML = formatDate(response.data.time * 1000);
     iconElement.setAttribute("src",response.data.condition.icon_url);
 
+
 }
 function search(city){
     let apiKey = "4bc76te01aac743d12o764377f46072e";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-    axios.get(apiUrl).then(displayTemprature);
+    axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event){
@@ -45,8 +47,32 @@ function handleSubmit(event){
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
 }
+function showFahrenheitTemperature(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celciusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function showCelciusTemperature(event){
+    event.preventDefault();
+    celciusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
 
-search("New York")
+}
+
+let celciusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature); 
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", showCelciusTemperature); 
+
+search("New York");
